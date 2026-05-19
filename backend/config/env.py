347a7a71a64600ae8a0ee3eeebafe50e65816cd5
1CodeBreaker705 <ranjansingh0661@gmail.com>
2026-05-19 23:29:54 +0530

    Update env.py

--- backend/config/env.py
+++ backend/config/env.py
@@ -6,7 +6,7 @@ load_dotenv()
 class ENVConfig:
    MONGO_URI=os.getenv("MONGO_URI","")
    MONGO_DB=os.getenv("MONGO_DB","")
-   JWT_AUTH_SECRET=os.getenv("JWT_AUTH_SECRET","a8fK29xQpL91mNzT7vYdR4sUwB2cXeH5")
+   JWT_AUTH_SECRET=os.getenv("JWT_AUTH_SECRET","PlaceholderJWTSecreta8fK29xQpL5")
    API_KEY_CLOUDINARY=os.getenv("API_KEY_CLOUDINARY","")
    API_SECRET_CLOUDINARY=os.getenv("API_SECRET_CLOUDINARY","")
    CLOUDINARY_CLOUD_NAME=os.getenv("CLOUDINARY_CLOUD_NAME","")
