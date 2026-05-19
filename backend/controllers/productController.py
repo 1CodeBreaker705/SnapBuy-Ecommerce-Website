cf2ffc16114709aa47c0298d6d1c750e217a0945
1CodeBreaker705 <ranjansingh0661@gmail.com>
2026-05-20 04:20:39 +0530

    Update productController.py

--- backend/controllers/productController.py
+++ backend/controllers/productController.py
@@ -3,11 +3,12 @@ from services import productService
 async def addProductController(images,data,user_id):
     return await productService.addProductService(images,data,user_id)
     
- 
 async def allProductsController(user_id):
     return await productService.allProductsService(user_id)
+
+async def updateProductController(id, images, data, user_id):
+    return await productService.updateProductService(id,images,data,user_id)
     
-  
 async def deleteProductController(id,user_id):
         return  await productService.deleteProductService(id,user_id)
         
